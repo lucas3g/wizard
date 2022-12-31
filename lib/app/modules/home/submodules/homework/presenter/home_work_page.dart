ab5df80f7e9e2f41ebe42264d744762317b9d353
lucas3g <lucassilva080397@gmail.com>
2022-12-30 23:27:27 -0300

    feat: add home work page

---
+++ lib/app/modules/home/submodules/homework/presenter/home_work_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeWorkPage extends StatefulWidget {
+  const HomeWorkPage({super.key});
+
+  @override
+  State<HomeWorkPage> createState() => _HomeWorkPageState();
+}
+
+class _HomeWorkPageState extends State<HomeWorkPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
