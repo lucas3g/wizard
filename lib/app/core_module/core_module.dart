ac13b34cb7ce5e08867bd848babcda90fd12774a
Lucas Silva <lucassilva080397@gmail.com>
2023-02-08 12:01:15 -0300

    fix: remove dio

--- lib/app/core_module/core_module.dart
+++ lib/app/core_module/core_module.dart
@@ -7,32 +7,11 @@ import 'package:wizard/app/core_module/services/supabase/supabase_service.dart';
 
 import 'services/shared_preferences/local_storage_interface.dart';
 import 'services/shared_preferences/shared_preferences_service.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-
-import 'services/client_http/client_http_interface.dart';
-import 'services/client_http/dio_client_http.dart';
-
-Bind<Dio> _dioFactory() {
-  final baseOptions = BaseOptions(
-    // baseUrl: baseUrl,
-    headers: {'Content-Type': 'application/json'},
-  );
-  return Bind.factory<Dio>((i) => Dio(baseOptions), export: true);
-}
 
 class CoreModule extends Module {
   @override
   final List<Bind> binds = [
-    //DIO
-    _dioFactory(),
-
-    //CLIENTHTTP
-    Bind.factory<IClientHttp>(
-      (i) => DioClientHttp(i()),
-      export: true,
-    ),
-
     //SHARED PREFERENCES
     AsyncBind<SharedPreferences>(
       (i) => SharedPreferences.getInstance(),
