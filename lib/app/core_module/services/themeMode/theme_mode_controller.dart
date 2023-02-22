2c7bcdf87483f3d70fb85e5317d4106e7736f9b4
lucas3g <lucassilva080397@gmail.com>
2023-02-21 22:19:01 -0300

    fix: themeMode controller

--- lib/app/core_module/services/themeMode/theme_mode_controller.dart
+++ lib/app/core_module/services/themeMode/theme_mode_controller.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:wizard/app/shared/stores/app_store.dart';
+import 'package:wizard/app/utils/navigation_service.dart';
 
 class ThemeModeController {
-  static ThemeMode get themeMode => Modular.get<AppStore>().themeMode.value;
+  static ThemeMode get themeMode =>
+      NavigationService.navigatorKey.currentContext!
+          .watch<AppStore>((store) => store.themeMode)
+          .themeMode
+          .value;
 }
