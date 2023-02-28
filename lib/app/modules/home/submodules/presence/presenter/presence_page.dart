391288ea4800a59f0130ea806a0e024eaf1c6e56
Lucas Silva <lucassilva080397@gmail.com>
2023-02-28 08:46:01 -0300

    fix: import presence

--- lib/app/modules/home/submodules/presence/presenter/presence_page.dart
+++ lib/app/modules/home/submodules/presence/presenter/presence_page.dart
@@ -3,7 +3,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
